a8e3110ead69bffb51939875f161f8592edb110a
NsysGit <naoki@n-system21.co.jp>
2019-12-11 15:19:22 +0900

    Initial Commit

---
+++ XCTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCTest2
+//
+//  Created by nsystem2 on 2019/12/11.
+//  Copyright © 2019 nsystem2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
